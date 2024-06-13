cf1bfb0dbee11fa37a3d47c78c56515e9dfade1a
SSShanu <sssnov2002@gmail.com>
2024-06-13 10:59:06 +0530

    Animation to the first componant of the homepage added

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -4,16 +4,14 @@ import NavBar from '../components/Navbar';
 import Footer from '../components/footer';
 import HomePageComponent from '../components/LeftSection';
 import Hero from '../components/hero';
-import Box from '../components/Box';
 import About from '../components/About';
 
 const Home = () => {
   return (
     <div>
       <NavBar />
-      <HomePageComponent />
       <Hero />
-      <Box />
+      <HomePageComponent />
       <About />
       
       <Footer />

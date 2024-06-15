374b4d0bd4865c19fbc1f4cbd2b7fae60841693a
iamB_praz <bprashurjya@gmail.com>
2024-06-15 23:28:35 +0530

    added links

--- frontend/src/components/hero.jsx
+++ frontend/src/components/hero.jsx
@@ -1,8 +1,15 @@
 import React from 'react';
 import Mainvideo from '../assests/video.mp4';
 import './Hero.css'; // Import the CSS file for styling
-
+import { useNavigate } from 'react-router-dom';
 const Hero = () => {
+
+    const navigate = useNavigate();
+
+    const handleNavigation = (path) => {
+      navigate(path);
+    };
+
   return (
     <div className='hero'>
         <video autoPlay loop muted id='video'>
@@ -15,8 +22,8 @@ const Hero = () => {
             </h1>
             <p>The Ultimate Online Code Editor with Integrated Courses and Challenges to Elevate Your Coding Skills</p>
             <div className='btn-group'>
-                <button className='btn'>Login</button>
-                <button className='btn btn-outline'>Sign up</button>
+                <button className='btn' onClick={() => handleNavigation('/login')}>Login</button>
+                <button className='btn btn-outline' onClick={() => handleNavigation('/signup')}>Sign up</button>
             </div>
         </div>
     </div>

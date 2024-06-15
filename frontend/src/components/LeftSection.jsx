02b66bec815ba7d578963af9a3c9919a6b04cbb7
SSShanu <sssnov2002@gmail.com>
2024-06-15 13:31:16 +0530

    Modified for having the slide down animation

--- frontend/src/components/LeftSection.jsx
+++ frontend/src/components/LeftSection.jsx
@@ -14,7 +14,28 @@ const HomePageComponent = () => {
                     <img src="https://th.bing.com/th/id/R.6bf341683642c2257a7ca6bb5c9e81f4?rik=OXWTg8M4RIlGFA&pid=ImgRaw&r=0" alt="Desc" />
                 </div>
             </div>
+            <div className="content">
+                <div className="text-container">
+                    <h1>Use the In-built Code-Editor.</h1>
+                    <p>Solve coding problems collaboratively in our development friendly environment with your friends and colleagues.</p>
+                    <button className="cta-button">Use our Code-Editor →</button>
+                </div>
+                <div className="image-container">
+                    <img src="https://th.bing.com/th/id/R.6bf341683642c2257a7ca6bb5c9e81f4?rik=OXWTg8M4RIlGFA&pid=ImgRaw&r=0" alt="Desc" />
+                </div>
+            </div>
+            <div className="content">
+                <div className="text-container">
+                    <h1>Use the In-built Code-Editor.</h1>
+                    <p>Solve coding problems collaboratively in our development friendly environment with your friends and colleagues.</p>
+                    <button className="cta-button">Use our Code-Editor →</button>
+                </div>
+                <div className="image-container">
+                    <img src="https://th.bing.com/th/id/R.6bf341683642c2257a7ca6bb5c9e81f4?rik=OXWTg8M4RIlGFA&pid=ImgRaw&r=0" alt="Desc" />
+                </div>
+            </div>
         </div>
+        
     );
 }
 

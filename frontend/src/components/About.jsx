00809a4c7c5351a3581e4b1820f4cd645d8cf7ff
Vivek4104 <vivekprajapati4104@gmail.com>
2024-06-16 01:15:36 +0530

    card editing done

--- frontend/src/components/About.jsx
+++ frontend/src/components/About.jsx
@@ -29,8 +29,11 @@ const About =() =>{
 export default About;*/
 import React from "react";
 import './About.css';
-import Aboutcard from "./Aboutcard";
 
+import logo from '../assests/vivek2.jpg';
+import logo1 from '../assests/Shanu.jpg';
+import logo2 from '../assests/wow.jpg';
+import logo3 from '../assests/Hari.jpg';
 const About = () => {
     return (
         <div className="about">
@@ -39,16 +42,64 @@ const About = () => {
                 <p>Crafting Excellence with Passion and Precision</p>
                 <div className="card-container">
                     <div className="card">
-                        <Aboutcard name="Prajpati vivek" education="2nd Year Btech" college="IIT Guwahati" location="Ahmedabad, Gujarat" />
+                    <div>
+            <div className="logo">
+                <img src={logo} alt="Logo" />
+            </div>
+            <h3>Prajpati Vivek</h3>
+            <ul>
+                <li>2nd Year Btech</li>
+                <li>IIT Guwahati</li>
+                <li>+917016990353</li>
+                <li><a href='https://www.linkedin.com/in/vivek-prajapati-4a4b20261'>linkekin</a></li>
+            </ul>
+        </div>
+                       
                     </div>
                     <div className="card">
-                        <Aboutcard name="Prajpati vivek" education="2nd Year Btech" college="IIT Guwahati" location="Ahmedabad, Gujarat" />
+                    <div>
+            <div className="logo">
+                <img src={logo2} alt="Logo" />
+            </div>
+            <h3>Prashurjya Bhagwati</h3>
+            <ul>
+                <li>2nd Year Btech</li>
+                <li>IIT Guwahati</li>
+                <li>+917637952281</li>
+                <li><a href='https://www.linkedin.com/in/prashurjya-bhagawati-b67770215/"'>linkekin</a></li>
+            </ul>
+        </div>
+                        
                     </div>
                     <div className="card">
-                        <Aboutcard name="Prajpati vivek" education="2nd Year Btech" college="IIT Guwahati" location="Ahmedabad, Gujarat" />
+                    <div>
+            <div className="logo">
+                <img src={logo1} alt="Logo" />
+            </div>
+            <h3>Shanu singh sengar</h3>
+            <ul>
+                <li>2nd Year Btech</li>
+                <li>IIT Guwahati</li>
+                <li>+918602271108</li>
+                <li><a href='https://www.linkedin.com/in/shanu-sengar-226b59261/'>linkekin</a></li>
+            </ul>
+        </div>
+                        
                     </div>
                     <div className="card">
-                        <Aboutcard name="Prajpati vivek" education="2nd Year Btech" college="IIT Guwahati" location="Ahmedabad, Gujarat" />
+                    <div>
+            <div className="logo">
+                <img src={logo3} alt="Logo" />
+            </div>
+            <h3>Harishankar Yadav</h3>
+            <ul>
+                <li>2nd Year Btech</li>
+                <li>IIT Guwahati</li>
+                <li>+917067817996</li>
+                <li><a href='https://www.linkedin.com/in/harishankar-yadav-7386a1280/'>linkekin</a></li>
+            </ul>
+        </div>
+                        
                     </div>
                 </div>
             </div>
